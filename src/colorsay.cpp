a6965d9a53a1f876e8b568ed4c24ca2a275ef484
Rafael Mohlin <raffe_mohlin@hotmail.com>
2016-10-01 00:30:39 +0200

    Added missing namespace

--- src/colorsay.cpp
+++ src/colorsay.cpp
@@ -11,139 +11,139 @@
 #include <sstream>
 
 using namespace std;
-
-ColorSayPlugin g_pPlugin;
+colorsay::ColorSayPlugin g_pPlugin;
 EXPOSE_SINGLE_INTERFACE_GLOBALVAR(ColorSayPlugin, IServerPluginCallbacks, INTERFACEVERSION_ISERVERPLUGINCALLBACKS, g_pPlugin);
 
+namespace colorsay {
 
-ColorSayPlugin::ColorSayPlugin() {}
-ColorSayPlugin::~ColorSayPlugin() {}
+    ColorSayPlugin::ColorSayPlugin() {}
+    ColorSayPlugin::~ColorSayPlugin() {}
 
-// Initialize the plugin to run
-// Return false if there is an error during startup.
-bool ColorSayPlugin::Load(CreateInterfaceFn interfaceFactory, CreateInterfaceFn gameServerFactory) {
-    INFO("Loading...");
-    ConnectTier1Libraries(&interfaceFactory, 1);
-    if(!colorsay::Globals::SetGlobals(interfaceFactory, gameServerFactory)) {
-        INFO("Plugin NOT loaded!");
-        return false;
+    // Initialize the plugin to run
+    // Return false if there is an error during startup.
+    bool ColorSayPlugin::Load(CreateInterfaceFn interfaceFactory, CreateInterfaceFn gameServerFactory) {
+        INFO("Loading...");
+        ConnectTier1Libraries(&interfaceFactory, 1);
+        if(!Globals::SetGlobals(interfaceFactory, gameServerFactory)) {
+            INFO("Plugin NOT loaded!");
+            return false;
+        }
+
+        INFO("Registering client console commands...");
+        clientcommands::register_commands();
+
+        INFO("Plugin loaded!");
+        return true;
     }
 
-    INFO("Registering client console commands...");
-    colorsay::clientcommands::register_commands();
+    // Called when the plugin should be shutdown
+    void ColorSayPlugin::Unload() {
+    }
 
-    INFO("Plugin loaded!");
-    return true;
-}
+    // called when a plugins execution is stopped but the plugin is not unloaded
+    void ColorSayPlugin::Pause() {
+    }
 
-// Called when the plugin should be shutdown
-void ColorSayPlugin::Unload() {
-}
+    // called when a plugin should start executing again (sometime after a Pause() call)
+    void ColorSayPlugin::UnPause() {
+    }
 
-// called when a plugins execution is stopped but the plugin is not unloaded
-void ColorSayPlugin::Pause() {
-}
+    // Returns string describing current plugin.  e.g., Admin-Mod.  
+    const char *ColorSayPlugin::GetPluginDescription() {
+        return PLUGIN_NAME " v" PLUGIN_VERSION " by burnedram (github.com/burnedram/csgo-plugin-color-say)";
+    }
 
-// called when a plugin should start executing again (sometime after a Pause() call)
-void ColorSayPlugin::UnPause() {
-}
+    // Called any time a new level is started (after GameInit() also on level transitions within a game)
+    void ColorSayPlugin::LevelInit(char const *pMapName) {
+    }
 
-// Returns string describing current plugin.  e.g., Admin-Mod.  
-const char *ColorSayPlugin::GetPluginDescription() {
-    return PLUGIN_NAME " v" PLUGIN_VERSION " by burnedram (github.com/burnedram/csgo-plugin-color-say)";
-}
+    // The server is about to activate
+    void ColorSayPlugin::ServerActivate(edict_t *pEdictList, int edictCount, int clientMax) {
+        Globals::maxPlayers = clientMax;
+    }
 
-// Called any time a new level is started (after GameInit() also on level transitions within a game)
-void ColorSayPlugin::LevelInit(char const *pMapName) {
-}
+    // The server should run physics/think on all edicts
+    void ColorSayPlugin::GameFrame(bool simulating) {
+    }
 
-// The server is about to activate
-void ColorSayPlugin::ServerActivate(edict_t *pEdictList, int edictCount, int clientMax) {
-    colorsay::Globals::maxPlayers = clientMax;
-}
+    // Called when a level is shutdown (including changing levels)
+    void ColorSayPlugin::LevelShutdown() {
+    }
 
-// The server should run physics/think on all edicts
-void ColorSayPlugin::GameFrame(bool simulating) {
-}
+    // Client is going active
+    void ColorSayPlugin::ClientActive(edict_t *pEntity) {
+    }
 
-// Called when a level is shutdown (including changing levels)
-void ColorSayPlugin::LevelShutdown() {
-}
+    // Client is fully connected (has received initial baseline of entities)
+    void ColorSayPlugin::ClientFullyConnect(edict_t *pEntity) {
+    }
 
-// Client is going active
-void ColorSayPlugin::ClientActive(edict_t *pEntity) {
-}
+    // Client is disconnecting from server
+    void ColorSayPlugin::ClientDisconnect(edict_t *pEntity) {
+    }
 
-// Client is fully connected (has received initial baseline of entities)
-void ColorSayPlugin::ClientFullyConnect(edict_t *pEntity) {
-}
+    // Client is connected and should be put in the game
+    void ColorSayPlugin::ClientPutInServer(edict_t *pEntity, char const *playername) {
+    }
 
-// Client is disconnecting from server
-void ColorSayPlugin::ClientDisconnect(edict_t *pEntity) {
-}
+    // Sets the client index for the client who typed the command into their console
+    void ColorSayPlugin::SetCommandClient(int index) {
+    }
 
-// Client is connected and should be put in the game
-void ColorSayPlugin::ClientPutInServer(edict_t *pEntity, char const *playername) {
-}
+    // A player changed one/several replicated cvars (name etc)
+    void ColorSayPlugin::ClientSettingsChanged(edict_t *pEdict) {
+    }
 
-// Sets the client index for the client who typed the command into their console
-void ColorSayPlugin::SetCommandClient(int index) {
-}
-
-// A player changed one/several replicated cvars (name etc)
-void ColorSayPlugin::ClientSettingsChanged(edict_t *pEdict) {
-}
-
-// Client is connecting to server (set retVal to false to reject the connection)
-//You can specify a rejection message by writing it into reject
-PLUGIN_RESULT ColorSayPlugin::ClientConnect(bool *bAllowConnect, edict_t *pEntity, const char *pszName, const char *pszAddress, char *reject, int maxrejectlen) {
-    return PLUGIN_CONTINUE;
-}
-
-// The client has typed a command at the console
-PLUGIN_RESULT ColorSayPlugin::ClientCommand(edict_t *pEdict, const CCommand &args) {
-    if(strcasecmp("colorsay", args.Arg(0)))
+    // Client is connecting to server (set retVal to false to reject the connection)
+    //You can specify a rejection message by writing it into reject
+    PLUGIN_RESULT ColorSayPlugin::ClientConnect(bool *bAllowConnect, edict_t *pEntity, const char *pszName, const char *pszAddress, char *reject, int maxrejectlen) {
         return PLUGIN_CONTINUE;
-    if(args.ArgC() == 1) {
-        ostringstream ss;
-        ss << "Usage: colorsay <command>\n";
-        colorsay::console::println(pEdict, ss.str().c_str());
-        return PLUGIN_STOP;
-    }
-    if(!colorsay::clientcommands::exists(args.Arg(1))) {
-        ostringstream ss;
-        ss << "Unknown command \"" << args.Arg(1) << "\"\n";
-        colorsay::console::println(pEdict, ss.str().c_str());
-        return PLUGIN_STOP;
     }
 
-    vector<string> stl_argv;
-    stl_argv.reserve(args.ArgC() - 1);
-    for(int i = 1; i < args.ArgC(); i++)
-        stl_argv.push_back(args.Arg(i));
+    // The client has typed a command at the console
+    PLUGIN_RESULT ColorSayPlugin::ClientCommand(edict_t *pEdict, const CCommand &args) {
+        if(strcasecmp("colorsay", args.Arg(0)))
+            return PLUGIN_CONTINUE;
+        if(args.ArgC() == 1) {
+            console::println(pEdict, "Usage: colorsay <command>");
+            return PLUGIN_STOP;
+        }
+        if(!clientcommands::exists(args.Arg(1))) {
+            ostringstream ss;
+            ss << "Unknown command \"" << args.Arg(1) << "\"";
+            console::println(pEdict, ss.str());
+            return PLUGIN_STOP;
+        }
 
-    string stl_args(args.ArgS() + strlen(args.Arg(1)));
-    size_t trim = stl_args.find_first_not_of(" \t");
-    if(trim != string::npos)
-        stl_args = stl_args.substr(trim);
+        vector<string> stl_argv;
+        stl_argv.reserve(args.ArgC() - 1);
+        for(int i = 1; i < args.ArgC(); i++)
+            stl_argv.push_back(args.Arg(i));
 
-    return colorsay::clientcommands::invoke(pEdict, stl_argv.front(), stl_args, stl_argv);
-}
+        string stl_args(args.ArgS() + strlen(args.Arg(1)));
+        size_t trim = stl_args.find_first_not_of(" \t");
+        if(trim != string::npos)
+            stl_args = stl_args.substr(trim);
 
-// A user has had their network id setup and validated 
-PLUGIN_RESULT ColorSayPlugin::NetworkIDValidated(const char *pszUserName, const char *pszNetworkID) {
-    return PLUGIN_CONTINUE;
-}
+        return clientcommands::invoke(pEdict, stl_argv.front(), stl_args, stl_argv);
+    }
 
-// This is called when a query from IServerPluginHelpers::StartQueryCvarValue is finished.
-// iCookie is the value returned by IServerPluginHelpers::StartQueryCvarValue.
-// Added with version 2 of the interface.
-void ColorSayPlugin::OnQueryCvarValueFinished(QueryCvarCookie_t iCookie, edict_t *pPlayerEntity, EQueryCvarValueStatus eStatus, const char *pCvarName, const char *pCvarValue) {
-}
+    // A user has had their network id setup and validated 
+    PLUGIN_RESULT ColorSayPlugin::NetworkIDValidated(const char *pszUserName, const char *pszNetworkID) {
+        return PLUGIN_CONTINUE;
+    }
 
-// added with version 3 of the interface.
-void ColorSayPlugin::OnEdictAllocated(edict_t *edict) {
-}
+    // This is called when a query from IServerPluginHelpers::StartQueryCvarValue is finished.
+    // iCookie is the value returned by IServerPluginHelpers::StartQueryCvarValue.
+    // Added with version 2 of the interface.
+    void ColorSayPlugin::OnQueryCvarValueFinished(QueryCvarCookie_t iCookie, edict_t *pPlayerEntity, EQueryCvarValueStatus eStatus, const char *pCvarName, const char *pCvarValue) {
+    }
 
-void ColorSayPlugin::OnEdictFreed(const edict_t *edict) {
+    // added with version 3 of the interface.
+    void ColorSayPlugin::OnEdictAllocated(edict_t *edict) {
+    }
+
+    void ColorSayPlugin::OnEdictFreed(const edict_t *edict) {
+    }
+
 }
--- src/colorsay.h
+++ src/colorsay.h
@@ -2,33 +2,33 @@
 
 #include <iserverplugin.h>
 
-class ColorSayPlugin : public IServerPluginCallbacks {
-    public:
-                                ColorSayPlugin();
-        virtual                 ~ColorSayPlugin();
+namespace colorsay {
+    class ColorSayPlugin : public IServerPluginCallbacks {
+        public:
+                                    ColorSayPlugin();
+            virtual                 ~ColorSayPlugin();
 
-        // IServerPluginCallbacks methods
-        virtual bool            Load(CreateInterfaceFn interfaceFactory, CreateInterfaceFn gameServerFactory );
-        virtual void            Unload();
-        virtual void            Pause();
-        virtual void            UnPause();
-        virtual const char     *GetPluginDescription();      
-        virtual void            LevelInit(char const *pszMapName);
-        virtual void            ServerActivate(edict_t *pEdictList, int edictCount, int clientMax);
-        virtual void            GameFrame(bool simulating);
-        virtual void            LevelShutdown();
-        virtual void            ClientActive(edict_t *pEntity);
-        virtual void            ClientFullyConnect(edict_t *pEntity);
-        virtual void            ClientDisconnect(edict_t *pEntity);
-        virtual void            ClientPutInServer(edict_t *pEntity, char const *pszPlayername);
-        virtual void            SetCommandClient(int index);
-        virtual void            ClientSettingsChanged(edict_t *pEdict);
-        virtual PLUGIN_RESULT   ClientConnect(bool *bAllowConnect, edict_t *pEntity, const char *pszName, const char *pszAddress, char *pszReject, int maxrejectlen);
-        virtual PLUGIN_RESULT   ClientCommand(edict_t *pEntity, const CCommand &args);
-        virtual PLUGIN_RESULT   NetworkIDValidated(const char *pszUserName, const char *pszNetworkID);
-        virtual void            OnQueryCvarValueFinished(QueryCvarCookie_t iCookie, edict_t *pPlayerEntity, EQueryCvarValueStatus eStatus, const char *pszCvarName, const char *pszCvarValue);
-        virtual void            OnEdictAllocated(edict_t *pEdict);
-        virtual void            OnEdictFreed(const edict_t *pEdict);  
-};
-
-extern void RegisterConCommands();
+            // IServerPluginCallbacks methods
+            virtual bool            Load(CreateInterfaceFn interfaceFactory, CreateInterfaceFn gameServerFactory );
+            virtual void            Unload();
+            virtual void            Pause();
+            virtual void            UnPause();
+            virtual const char     *GetPluginDescription();      
+            virtual void            LevelInit(char const *pszMapName);
+            virtual void            ServerActivate(edict_t *pEdictList, int edictCount, int clientMax);
+            virtual void            GameFrame(bool simulating);
+            virtual void            LevelShutdown();
+            virtual void            ClientActive(edict_t *pEntity);
+            virtual void            ClientFullyConnect(edict_t *pEntity);
+            virtual void            ClientDisconnect(edict_t *pEntity);
+            virtual void            ClientPutInServer(edict_t *pEntity, char const *pszPlayername);
+            virtual void            SetCommandClient(int index);
+            virtual void            ClientSettingsChanged(edict_t *pEdict);
+            virtual PLUGIN_RESULT   ClientConnect(bool *bAllowConnect, edict_t *pEntity, const char *pszName, const char *pszAddress, char *pszReject, int maxrejectlen);
+            virtual PLUGIN_RESULT   ClientCommand(edict_t *pEntity, const CCommand &args);
+            virtual PLUGIN_RESULT   NetworkIDValidated(const char *pszUserName, const char *pszNetworkID);
+            virtual void            OnQueryCvarValueFinished(QueryCvarCookie_t iCookie, edict_t *pPlayerEntity, EQueryCvarValueStatus eStatus, const char *pszCvarName, const char *pszCvarValue);
+            virtual void            OnEdictAllocated(edict_t *pEdict);
+            virtual void            OnEdictFreed(const edict_t *pEdict);  
+    };
+}
